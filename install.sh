b862a223eb38fbd2ca967d04290cc538a5564c00
your username <your email>
2026-03-07 17:53:36 +0900

    minor changes

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 set -e
 
 # bash
-ln -sf ~/dotfiles/bash/bashrc ~/.bashrc             # only for containers
-ln -sf ~/dotfiles/bash/bash_aliases ~/.bash_aliases # only for containers
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc             # only for containers
+ln -sf ~/dotfiles/bash/.bash_aliases ~/.bash_aliases # only for containers
 
 # others
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
@@ -11,6 +11,6 @@ ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
 # vscode
-ln -sf ~/dotfiles/vscode/settins.json ~/.config/Code/User/settings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 while read line; do code --install-extension "$line"; done < vscode/extensions.txt
