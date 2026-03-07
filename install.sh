8b854118cdea90826751eab1c7c9c6e28c0d3d59
your username <your email>
2026-03-07 17:00:54 +0900

    remove: nvim and wezterm bits

--- install.sh
+++ install.sh
@@ -6,9 +6,3 @@ ln -sf ~/dotfiles/bash/bashrc ~/.bashrc             # only for containers
 ln -sf ~/dotfiles/bash/bash_aliases ~/.bash_aliases # only for containers
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-
-# directories (must remove directory manually first)
-rm -rf ~/.config/nvim
-ln -s ~/dotfiles/nvim ~/.config/nvim
-rm -rf ~/.config/wezterm
-ln -s ~/dotfiles/wezterm ~/.config/wezterm
