ac26891f4e32033100173c83826bf802410fc7f8
ShuShiotani71 <shu.shiotani70@gmail.com>
2026-04-11 17:17:28 +0900

    refine: some stuffs

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir -p ~/.config/Code/User
 ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
 while read line; do code --install-extension "$line"; done < vscode/extensions.txt
